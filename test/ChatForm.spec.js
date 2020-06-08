e2dfe5ec41f4a1639f2289659d15b1db98096fa7
IsuriVidyarathne <isuri.vidyarathne@gsts.ca>
2020-06-08 14:43:57 -0400

    ChatForm test

---
+++ test/ChatForm.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils';
+import ChatForm from './ChatForm.vue';
+
+/*describe('ChatForm.vue', () ={
+    test('Testing if it writes that user is writing a message', () =>[
+        const wrapper = mount(ChatForm)
+        expect(wrapper.text()).toContain("Text is required");
+    ]
+    )
+})*/
